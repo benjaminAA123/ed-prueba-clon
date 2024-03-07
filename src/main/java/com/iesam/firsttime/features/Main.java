2ceceaefe86db59e286d500892ca5e48e90aae6f
Chema <josem.perram@educa.jcyl.es>
2024-03-07 15:58:05 +0100

    Estructura de test

--- src/main/java/com/iesam/firsttime/features/Main.java
+++ src/main/java/com/iesam/firsttime/features/Main.java
@@ -1,8 +1,14 @@
 package com.iesam.firsttime.features;
 
+import com.iesam.firsttime.features.appconfig.domain.AppConfig;
 import com.iesam.firsttime.features.appconfig.presentation.AppConfigPresentation;
 import com.iesam.firsttime.features.vote.presentation.VotePresentation;
 import com.iesam.firsttime.features.welcome.presentation.WelcomePresentation;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
 
 public class Main {
 
